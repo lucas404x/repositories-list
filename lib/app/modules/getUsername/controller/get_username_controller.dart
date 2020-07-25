0395944583c96ab0eb6ee4b8b6e39dcd2860cf71
José Lucas <joselucas0303@gmail.com>
2020-07-25 19:10:52 -0300

    get_username_controller created

---
+++ lib/app/modules/getUsername/controller/get_username_controller.dart
@@ -0,0 +1,7 @@
+import 'package:mobx/mobx.dart';
+part 'get_username_controller.g.dart';
+
+class GetUsernameController = _GetUsernameControllerBase
+    with _$GetUsernameController;
+
+abstract class _GetUsernameControllerBase with Store {}
