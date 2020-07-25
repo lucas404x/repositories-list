dc9288feb4c105e950ca3ed2b8700b400fed8ae5
Lucas <joselucas0303@gmail.com>
2020-07-25 13:13:47 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## This app was made to train pull requests and merge.
---
+++ android/app/src/main/kotlin/br/lucas404x/repositories_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.lucas404x.repositories_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
