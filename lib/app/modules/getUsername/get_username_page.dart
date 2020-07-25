6aa0f0435ffae19cac6031b7a04a6082baa2cd37
José Lucas <joselucas0303@gmail.com>
2020-07-25 19:10:33 -0300

    get_username_page created

---
+++ lib/app/modules/getUsername/get_username_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class GetUsernamePage extends StatefulWidget {
+  @override
+  _GetUsernamePageState createState() => _GetUsernamePageState();
+}
+
+class _GetUsernamePageState extends State<GetUsernamePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
