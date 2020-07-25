c29bf001e76e9bde13a3916113dc94c1bad5b2a6
José Lucas <joselucas0303@gmail.com>
2020-07-25 19:11:53 -0300

    Router's module replaced to GetUserModule

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,10 +1,10 @@
 import 'package:dio/dio.dart';
+import 'modules/getUsername/get_username_module.dart';
 import 'shared/repositories/github_repository.dart';
 import 'shared/repositories/interfaces/github_repository_interface.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter/material.dart';
 import 'package:repositories_list/app/app_widget.dart';
-import 'package:repositories_list/app/modules/home/home_module.dart';
 
 class AppModule extends MainModule {
   @override
@@ -17,7 +17,7 @@ class AppModule extends MainModule {
 
   @override
   List<Router> get routers => [
-        Router(Modular.initialRoute, module: HomeModule()),
+        Router(Modular.initialRoute, module: GetUsernameModule()),
       ];
 
   @override
