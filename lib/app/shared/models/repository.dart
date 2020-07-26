ecdba15d15727d1be69c10f5503a61014ded169d
José Lucas <joselucas0303@gmail.com>
2020-07-26 11:01:42 -0300

    removed RepositoryModel's commits attribute

--- lib/app/shared/models/repository.dart
+++ lib/app/shared/models/repository.dart
@@ -3,14 +3,8 @@ class RepositoryModel {
   String description;
   int stars;
   int forks;
-  int commits;
   int watchers;
 
   RepositoryModel(
-      {this.name,
-      this.description,
-      this.stars,
-      this.forks,
-      this.commits,
-      this.watchers});
+      {this.name, this.description, this.stars, this.forks, this.watchers});
 }
