13d000030600771fc5d134587ce831b27cb4563d
José Lucas <joselucas0303@gmail.com>
2020-07-26 14:53:58 -0300

    controller object added in binds list

--- lib/app/modules/getUsername/get_username_module.dart
+++ lib/app/modules/getUsername/get_username_module.dart
@@ -1,11 +1,16 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:repositories_list/app/modules/getUsername/controller/get_username_controller.dart';
 import 'package:repositories_list/app/modules/getUsername/get_username_page.dart';
+import 'package:repositories_list/app/shared/repositories/github_repository.dart';
 
 class GetUsernameModule extends ChildModule {
   @override
   List<Bind> get binds => [
-        Bind((i) => GetUsernameController()),
+        Bind((i) => GetUsernameController(GithubRepository(
+                Dio(BaseOptions(baseUrl: "https://api.github.com", headers: {
+              "Accept": "application/vnd.github.v3+json",
+            })))))
       ];
 
   @override
