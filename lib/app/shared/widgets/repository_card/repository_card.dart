edb7d73a0aa68b6714744d59db9b8af3337b7a0b
José Lucas <joselucas0303@gmail.com>
2020-07-27 17:19:59 -0300

    added Function that pass the data to the Repo Details

--- lib/app/shared/widgets/repository_card/repository_card.dart
+++ lib/app/shared/widgets/repository_card/repository_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:repositories_list/app/shared/models/repository_model.dart';
 
 class RepositoryCard extends StatelessWidget {
@@ -10,10 +11,14 @@ class RepositoryCard extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return Container(
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12), color: Colors.white),
-      height: size.height * .3,
+    return GestureDetector(
+      onTap: () =>
+          Modular.to.pushNamed('/home/repoDetails', arguments: repositoryModel),
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12), color: Colors.white),
+        height: size.height * .3,
+      ),
     );
   }
 }
