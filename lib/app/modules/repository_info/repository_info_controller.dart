5cb0180a51a68b4b39ac7b837df35cc5e9d2bae1
bmsrangel <bmsrangel@hotmail.com>
2020-07-29 12:05:36 -0300

    Fixed an error where subscribers_count was null and refactored UserDataStore injection

--- lib/app/modules/repository_info/repository_info_controller.dart
+++ lib/app/modules/repository_info/repository_info_controller.dart
@@ -1,4 +1,7 @@
 import 'package:mobx/mobx.dart';
+import 'package:repositories_list/app/shared/models/repository_model.dart';
+import 'package:repositories_list/app/shared/repositories/interfaces/github_repository_interface.dart';
+import 'package:repositories_list/app/shared/stores/user_data_store.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 part 'repository_info_controller.g.dart';
@@ -7,6 +10,26 @@ class RepositoryInfoController = _RepositoryInfoControllerBase
     with _$RepositoryInfoController;
 
 abstract class _RepositoryInfoControllerBase with Store {
+  final IGithubRepository _githubRepository;
+  final RepositoryModel _repositoryModel;
+  final UserDataStore _userDataStore;
+
+  @observable
+  RepositoryModel repositoryData;
+
+  _RepositoryInfoControllerBase(
+      this._githubRepository, this._repositoryModel, this._userDataStore) {
+    this._init();
+  }
+
+  @action
+  _init() async {
+    try {
+      this.repositoryData = await this._githubRepository.getRepositoryInfo(
+          this._userDataStore.userData.login, this._repositoryModel.name);
+    } catch (e) {}
+  }
+
   void openUrl(String gitRepoLink) async {
     String url = _convertToUrl(gitRepoLink);
     if (await canLaunch(url)) {
--- lib/app/modules/repository_info/repository_info_module.dart
+++ lib/app/modules/repository_info/repository_info_module.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:repositories_list/app/shared/repositories/interfaces/github_repository_interface.dart';
+import 'package:repositories_list/app/shared/stores/user_data_store.dart';
 
 import 'repository_info_controller.dart';
 import 'repository_info_page.dart';
@@ -6,15 +8,16 @@ import 'repository_info_page.dart';
 class RepositoryInfoModule extends ChildModule {
   @override
   List<Bind> get binds => [
-        Bind((i) => RepositoryInfoController()),
+        Bind((i) => RepositoryInfoController(
+              Modular.get<IGithubRepository>(),
+              i.args.data,
+              Modular.get<UserDataStore>(),
+            )),
       ];
 
   @override
   List<Router> get routers => [
-        Router(Modular.initialRoute,
-            child: (_, args) => RepositoryInfoPage(
-                  repository: args.data,
-                )),
+        Router(Modular.initialRoute, child: (_, args) => RepositoryInfoPage()),
       ];
 
   static Inject get to => Inject<RepositoryInfoModule>.of();
